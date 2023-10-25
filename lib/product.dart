8ed816fd0c05b5b727ac574b3d599e973a017ca0
aji_kim <kaj981020@gmail.com>
2023-10-25 15:51:57 +0900

    아지 > 오류잡는 중

--- lib/product.dart
+++ lib/product.dart
@@ -4,71 +4,18 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '메인',
-      home: Main(),
-    );
-  }
-}
-
-class Main extends StatefulWidget {
-  const Main({Key? key});
-
-  @override
-  State<Main> createState() => _MainState();
-}
-
-class _MainState extends State<Main> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("용채")),
-        body: Container(),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Product()),
-                  );
-                },
-                icon: const Icon(Icons.add_circle_outline),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class Product extends StatefulWidget {
   @override
   _ProductState createState() => _ProductState();
 }
 
 class _ProductState extends State<Product> {
-  late Stream<QuerySnapshot> productStream; // Firestore 스트림 변수 추가
+  late Stream<QuerySnapshot> productStream;
 
   @override
   void initState() {
     super.initState();
-    productStream = FirebaseFirestore.instance.collection("product").snapshots(); // Firestore 스트림 초기화
+    productStream = FirebaseFirestore.instance.collection("product").snapshots();
   }
 
   @override
@@ -79,14 +26,14 @@ class _ProductState extends State<Product> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context); // 뒤로가기 버튼을 누를 때 이전 페이지로 돌아가도록 설정
+            Navigator.pop(context);
           },
         ),
       ),
       body: Container(
         padding: const EdgeInsets.all(10),
         child: StreamBuilder<QuerySnapshot>(
-          stream: productStream, // Firestore 스트림 사용
+          stream: productStream,
           builder: (context, snapshot) {
             if (snapshot.hasError) {
               return Text('Error: ${snapshot.error}');
@@ -110,7 +57,8 @@ class _ProductState extends State<Product> {
               itemBuilder: (context, index) {
                 final document = snapshot.data!.docs[index];
                 final productName = document['product_name'] as String;
-                final imageUrl = document['image_url'] as String; // 이미지 URL 가져오기
+                final price = document['price'] as String; // 가격 가져오기
+                final imageUrl = document['image_url'] as String;
 
                 return Container(
                   width: 100,
@@ -121,7 +69,7 @@ class _ProductState extends State<Product> {
                   ),
                   child: Stack(
                     children: [
-                      Image.network( // 이미지 로드
+                      Image.network(
                         imageUrl,
                         width: double.infinity,
                         height: double.infinity,
@@ -130,12 +78,24 @@ class _ProductState extends State<Product> {
                       Positioned(
                         bottom: 8,
                         left: 8,
-                        child: Text(
-                          productName,
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                          ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              productName,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                              ),
+                            ),
+                            Text(
+                              '가격: $price 원',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
