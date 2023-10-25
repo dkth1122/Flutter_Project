4cfb16f323a92b5a592b632aed3a0d595e0db90f
eksqjf <129019461+eksqjf@users.noreply.github.com>
2023-10-25 14:26:18 +0900

    main commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,52 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:project_flutter/product.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '메인',
-      home: Main(),
+      home: MyHomePage(),
     );
   }
 }
 
-class Main extends StatefulWidget {
-  const Main({super.key});
-
+class MyHomePage extends StatefulWidget {
   @override
-  State<Main> createState() => _MainState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MainState extends State<Main> {
+class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("용채"),),
-        body: Container(),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                  onPressed: (){
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (context) => Product())
-                    );
-                  },
-                  icon: Icon(Icons.add_circle_outline)
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('무한 반복 애니메이션'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: (){
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Product())
+                  );
+                },
+                icon: Icon(Icons.add_circle_outline)
+            )
+          ],
         ),
       ),
     );
