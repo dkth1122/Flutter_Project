de86add8b5399726262f5a903c2482c0e3e16ffc
eksqjf <129019461+eksqjf@users.noreply.github.com>
2023-10-25 15:40:16 +0900

    main yaml commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,119 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:project_flutter/product.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '메인',
-      home: Main(),
-    );
+    return const MaterialApp(title: 'Flutter App', home: HomePage());
   }
 }
 
-class Main extends StatefulWidget {
-  const Main({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<Main> createState() => _MainState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MainState extends State<Main> {
-  late PageController _pageController;
-  int _currentPage = 0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // 페이지 컨트롤러 초기화
-    _pageController = PageController(
-      initialPage: _currentPage,
-      viewportFraction: 0.8,
-    );
-
-    // 타이머를 사용하여 자동 슬라이드 구현
-    Timer.periodic(Duration(seconds: 2), (timer) {
-      if (_currentPage < 2) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
-      }
-      _pageController.animateToPage(
-        _currentPage,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
+class _HomePageState extends State<HomePage> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+  List imageList = [
+    "https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg",
+    "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_1280.jpg",
+    "https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830_1280.jpg",
+    "https://cdn.pixabay.com/photo/2015/06/19/20/13/sunset-815270_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_1280.jpg",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("용채"),
+        title: const Text('Carousel Slide'),
       ),
-      body: Center(
-        child: ListView(
-          children: [
-            Container(
-              height: 200, // 슬라이드 높이 설정
-              child: PageView.builder(
-                controller: _pageController,
-                onPageChanged: (index) {
-                  setState(() {
-                    _currentPage = index;
-                  });
-                },
-                itemBuilder: (BuildContext context, int index) {
-                  return SizedBox(
-                    width: 100,
-                    child: Container(
-                      color: Colors.lightBlue,
-                      margin: EdgeInsets.symmetric(horizontal: 10.0), // 슬라이드 간 간격
-                    ),
-                  );
-                },
-                itemCount: 3, // 슬라이드 개수
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            child: Stack(
+              children: [
+                sliderWidget(),
+                sliderIndicator(),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text("Welcome to the carousel slide app",
+                style: TextStyle(fontSize: 18)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget sliderWidget() {
+    return CarouselSlider(
+      carouselController: _controller,
+      items: imageList.map(
+            (imgLink) {
+          return Builder(
+            builder: (context) {
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Image(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                    imgLink,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ).toList(),
+      options: CarouselOptions(
+        height: 300,
+        viewportFraction: 1.0,
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 4),
+        onPageChanged: (index, reason) {
+          setState(() {
+            _current = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget sliderIndicator() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: imageList.asMap().entries.map((entry) {
+          return GestureDetector(
+            onTap: () => _controller.animateToPage(entry.key),
+            child: Container(
+              width: 12,
+              height: 12,
+              margin:
+              const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color:
+                Colors.white.withOpacity(_current == entry.key ? 0.9 : 0.4),
               ),
             ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Product()),
-                );
-              },
-              icon: Icon(Icons.add_circle_outline),
-            )
-          ],
-        ),
+          );
+        }).toList(),
       ),
     );
   }
