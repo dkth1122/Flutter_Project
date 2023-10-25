a2e7b24f5bccb8e9a8ede5af3541d9e0e24bc530
dkth <skdus2995@naver.com>
2023-10-25 12:21:22 +0900

    chat 수정 중 (일방향 유저 채팅 기능 구현)

--- lib/chat/chat.dart
+++ lib/chat/chat.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:project_flutter/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(Chat());
 }
 
@@ -24,32 +27,39 @@ class _ChatState extends State<Chat> {
     messageController = TextEditingController();
   }
 
-  Widget _chatList(){
+  Widget _chatList() {
     return StreamBuilder(
-      //호출한 데이터를 stream에 담아야함
-      //딱 한번만 호출 하려면 : 메소드 체이닝 기법으로 FirebaseFirestore.instance.collection("users").snapshots(), 로 담기 가능
-      //여러 번 사용하려면 메소드로 만들어서 따로 뺴야 함
-        stream: FirebaseFirestore.instance.collection("chat").snapshots(),
-        //데이터 실시간 처리를 위해 단순한 스냅샷이 아닌 async 스냅샷으로 지정
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snap){
-          //------위에 까지는 데이터 호출
+      stream: FirebaseFirestore.instance.collection("chat").snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snap) {
+        if (snap.connectionState == ConnectionState.waiting) {
+          // 여기서는 데이터가 아직 도착하지 않았을 때의 상황을 처리합니다.
+          return CircularProgressIndicator(); // 로딩 표시나 다른 대체 UI를 보여줄 수 있습니다.
+        }
+
+        if (snap.hasError) {
+          // 오류 처리
+          return Text('Error: ${snap.error}');
+        }
+
+        // snap.data가 null이 아닌지 확인
+        if (snap.data != null) {
           return ListView(
-            children:
-            //호출 결과 자체가 List형태라서 칠드런의 [] 제거 가능
-            //map안에 파라미터를 가지는 콜백 함수 호출
-            snap.data!.docs.map((DocumentSnapshot document){
-              //문서에서 데이터를 가져와서 data 변수에 담음 => 이 데이터는 맵 형식으로 표현됨
-              //<key, value>
+            children: snap.data!.docs.map((DocumentSnapshot document) {
               Map<String, dynamic> data = document.data() as Map<String, dynamic>;
               return ListTile(
-                title: Text(data['name']),
-                subtitle: Text("나이 : ${data['age']}"),
+                title: Text(data['text']),
+                subtitle: Text("작성일 : ${data['sendTime'].toDate().toString()}"),
               );
             }).toList(),
           );
+        } else {
+          // 데이터가 null이면 빈 화면을 보여줄 수 있습니다.
+          return Text('No data available.');
         }
+      },
     );
   }
+
 
 
   @override
@@ -62,36 +72,7 @@ class _ChatState extends State<Chat> {
         body: Column(
           children: [
             Expanded(
-              child: StreamBuilder(
-                stream: FirebaseFirestore.instance
-                    .collection('chat')
-                    .orderBy('sendTime', descending: true)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  } else {
-                    messages = (snapshot.data?.docs ?? []).map((doc) => doc.data() as Map<String, dynamic>).toList();
-                    print(messages);
-                    return ListView.builder(
-                      itemCount: messages.length,
-                      itemBuilder: (context, index) {
-                        final message = messages[index];
-                        return ListTile(
-                          title: Text(message['text']),
-                          subtitle: Text(message['user']),
-                        );
-                      },
-                    );
-                  }
-                },
-              ),
+              child:_chatList()
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
