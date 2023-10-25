84f15db5f07f7ceea8af3e9e73c259d2238d256b
aji_kim <kaj981020@gmail.com>
2023-10-25 12:22:28 +0900

    아지 > 상품페이지 이미지

--- lib/product.dart
+++ lib/product.dart
@@ -8,13 +8,15 @@ class Product extends StatefulWidget {
 }
 
 class _ProductState extends State<Product> {
-  List<String> path = ['dog1.PNG', 'dog2.PNG', 'dog3.PNG'];
+  List<String> path = ['dog1.PNG','dog2.PNG','dog3.PNG',  ];
+  List<String> productNames = ['뽀짝이 \n25,000', '꼬순이 \n25,000', '시잡이 \n25,000']; // 각 이미지에 대한 텍스트
   List<bool> isFavoriteList = [false, false, false];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(title: Text("상품페이지"),),
         body: Container(
           padding: EdgeInsets.all(10),
           child: GridView.builder(
@@ -30,11 +32,27 @@ class _ProductState extends State<Product> {
                 height: 100,
                 padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 1.0), // 검정색 보더 선 추가
+                  border: Border.all(color: Colors.black, width: 1.0),
                 ),
                 child: Stack(
                   children: [
-                    Image.asset(path[index], fit: BoxFit.fill),
+                    Image.asset(
+                      path[index],
+                      width: double.infinity,
+                      height: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      bottom: 8,
+                      left: 8,
+                      child: Text(
+                        productNames[index], // 각 이미지에 대한 텍스트 설정
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
                     Positioned(
                       top: 8,
                       left: 8,
