8789817f313a61c5be06ee4db00d069004aa0412
aji_kim <kaj981020@gmail.com>
2023-10-25 11:45:53 +0900

    아지 > 상품페이지

--- lib/product.dart
+++ lib/product.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Product extends StatelessWidget {
-  Product({super.key});
-  List<String> path = [ 'azi1.jpg', 'azi2.jpg', 'azi3.jpg'];
+class Product extends StatefulWidget {
+  Product({Key? key});
+
+  @override
+  _ProductState createState() => _ProductState();
+}
+
+class _ProductState extends State<Product> {
+  List<String> path = ['dog1.PNG', 'dog2.PNG', 'dog3.PNG'];
+  List<bool> isFavoriteList = [false, false, false];
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +17,45 @@ class Product extends StatelessWidget {
       home: Container(
         padding: EdgeInsets.all(10),
         child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8
-            ),
-            itemCount: path.length,
-            itemBuilder: (context, index){
-              return Container(
-                padding: EdgeInsets.all(10),
-                child: Image.asset(path[index], fit : BoxFit.fill),
-
-              );
-            }),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+          ),
+          itemCount: path.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: EdgeInsets.all(10),
+              child: Stack(
+                children: [
+                  Image.asset(path[index], fit: BoxFit.fill),
+                  Positioned(
+                    top: 8,
+                    left: 8,
+                    child: IconButton(
+                      icon: Icon(
+                        isFavoriteList[index]
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color: Colors.red,
+                        size: 16,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isFavoriteList[index] = !isFavoriteList[index];
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
 }
+
+
 
