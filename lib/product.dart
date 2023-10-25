d1090a8227fcbaf1e069e6a4128b379f3d5de8fa
aji_kim <kaj981020@gmail.com>
2023-10-25 14:56:00 +0900

    아지 > 오류

--- lib/product.dart
+++ lib/product.dart
@@ -1,14 +1,59 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project_flutter/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(Product());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '메인',
+      home: Main(),
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({Key? key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("용채")),
+        body: Container(),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Product()),
+                  );
+                },
+                icon: Icon(Icons.add_circle_outline),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class Product extends StatefulWidget {
@@ -17,82 +62,105 @@ class Product extends StatefulWidget {
 }
 
 class _ProductState extends State<Product> {
-  List<String> path = ['dog1.PNG','dog2.PNG','dog3.PNG', 'dog4.png'];
-  List<String> productNames = ['뽀짝이 \n25,000', '꼬순이 \n25,000', '시잡이 \n25,000', '쒸익이 \n25,000']; // 각 이미지에 대한 텍스트
   List<bool> isFavoriteList = [false, false, false, false];
+  late Stream<QuerySnapshot> productStream; // Firestore 스트림 변수 추가
+
+  @override
+  void initState() {
+    super.initState();
+    productStream = FirebaseFirestore.instance.collection("product").snapshots(); // Firestore 스트림 초기화
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("상품페이지"),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context); // 뒤로가기 버튼을 누를 때 이전 페이지로 돌아가도록 설정
-            },
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("상품페이지"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // 뒤로가기 버튼을 누를 때 이전 페이지로 돌아가도록 설정
+          },
         ),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-            ),
-            itemCount: path.length,
-            itemBuilder: (context, index) {
-              return Container(
-                width: 100,
-                height: 100,
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 1.0),
-                ),
-                child: Stack(
-                  children: [
-                    Image.asset(
-                      path[index],
-                      width: double.infinity,
-                      height: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                    Positioned(
-                      bottom: 8,
-                      left: 8,
-                      child: Text(
-                        productNames[index], // 각 이미지에 대한 텍스트 설정
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: productStream, // Firestore 스트림 사용
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return Text('상품이 없습니다.');
+            }
+
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final document = snapshot.data!.docs[index];
+                final productName = document['product_name'] as String;
+                final imageUrl = document['image_url'] as String; // 이미지 URL 가져오기
+                final isFavorite = isFavoriteList[index];
+
+                return Container(
+                  width: 100,
+                  height: 100,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1.0),
+                  ),
+                  child: Stack(
+                    children: [
+                      Image.network( // 이미지 로드
+                        imageUrl,
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        bottom: 8,
+                        left: 8,
+                        child: Text(
+                          productName,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 8,
-                      left: 8,
-                      child: IconButton(
-                        icon: Icon(
-                          isFavoriteList[index]
-                              ? Icons.favorite
-                              : Icons.favorite_border,
-                          color: Colors.red,
-                          size: 24,
+                      Positioned(
+                        top: 8,
+                        left: 8,
+                        child: IconButton(
+                          icon: Icon(
+                            isFavorite ? Icons.favorite : Icons.favorite_border,
+                            color: Colors.red,
+                            size: 24,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              isFavoriteList[index] = !isFavorite;
+                            });
+                          },
                         ),
-                        onPressed: () {
-                          setState(() {
-                            isFavoriteList[index] = !isFavoriteList[index];
-                          });
-                        },
                       ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
