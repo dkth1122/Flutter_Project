a0322ddccc397575849e660b02ef71f61dcc5c8b
yny <skdus2995@naver.com>
2023-10-31 00:34:10 +0900

    expert page 수익관리 페이지 (틀 잡기, 더미데이터 입력 완료 -> 추후 결제 완료 후 수익 컬렉션에 데이터 넣도록 설정 필요)

--- lib/expert/revenue.dart
+++ lib/expert/revenue.dart
@@ -7,75 +7,212 @@ class Revenue extends StatefulWidget {
 }
 
 class _RevenueState extends State<Revenue> {
-  List<String> dates = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
-  List<double> earnings = [1000, 1500, 1200, 2200, 1800, 2100, 2300];
+  List<String> months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  List<double> earnings = [1000, 1500, 1200, 2200, 1800, 2100, 2300, 1000, 1500, 1200, 2200, 1800];
+  double availableEarnings = 1000.0;
+  double expectedEarnings = 2000.0;
+  double completedWithdrawals = 500.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('수익 관리'),
+          backgroundColor: Colors.white, // 배경색
+          elevation: 0, // 그림자 효과 제거
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black, // 좌측 아이콘 색상
+            ),
+            onPressed: () {
+              // 좌측 아이콘을 눌렀을 때 수행할 작업
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.black, // 검색 아이콘 색상
+              ),
+              onPressed: () {
+                // 검색 아이콘을 눌렀을 때 수행할 작업
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.black, // 알림 아이콘 색상
+              ),
+              onPressed: () {
+                // 알림 아이콘을 눌렀을 때 수행할 작업
+              },
+            ),
+          ],
+          title: Text(
+            '수익관리',
+            style: TextStyle(
+              color: Colors.black, // 타이틀 색상
+              fontSize: 24.0, // 타이틀 폰트 크기
+            ),
+          ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('수익 그래프'),
             Container(
-              height: 300,
-              child: BarChart(
-                BarChartData(
-                  gridData: FlGridData(show: false),
-                  titlesData: FlTitlesData(
-                    leftTitles: SideTitles(
-                      showTitles: true,
-                      getTitles: (value) {
-                        double minEarnings = 1000;
-                        double maxEarnings = 2500;
-                        for (var i = minEarnings; i <= maxEarnings; i += 500) {
-                          if (value == i) {
-                            return i.toStringAsFixed(0);
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey[300]!, // Container의 외곽 보더 색상
+                  width: 1.0,              // Container의 외곽 보더 두께
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildStatRow("출금 가능 수익금", availableEarnings),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // 출금 신청 로직을 추가
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.amber, // 버튼 색상 설정
+                        ),
+                        child: Text('출금 신청'),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          // 출금 취소 로직을 추가
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.grey, // 버튼 색상 설정
+                        ),
+                        child: Text('출금 취소'),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    color: Colors.grey[300]!, // 구분선의 색상
+                    thickness: 1.0,            // 구분선의 두께
+                  ),
+                  _buildStatRow("예상 수익금", expectedEarnings),
+                  Divider(
+                    color: Colors.grey[300]!, // 구분선의 색상
+                    thickness: 1.0,            // 구분선의 두께
+                  ),
+                  _buildStatRow("출금 완료 수익금", completedWithdrawals),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                height: 300,
+                width: 500,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[300]!, // Container의 외곽 보더 색상
+                    width: 1.0,              // Container의 외곽 보더 두께
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                padding: const EdgeInsets.all(16.0),
+                child: BarChart(
+                  BarChartData(
+                    titlesData: FlTitlesData(
+                      leftTitles: SideTitles(
+                        showTitles: true,
+                        //Y축에 담을 정보 내용
+                        getTextStyles: (context, value) => TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                        ),
+                        getTitles: (value) {
+                          if (value % 500 == 0) {
+                            return value.toInt().toString();
                           }
-                        }
-                        return '';
-                      },
+                          return '';
+                        },
+                      ),
+                      bottomTitles: SideTitles(
+                        showTitles: true,
+                        getTextStyles: (context, value) => TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                        getTitles: (value) {
+                          int index = value.toInt();
+                          if (index >= 0 && index < months.length) {
+                            return months[index];
+                          }
+                          return '';
+                        },
+                      ),
                     ),
-                    bottomTitles: SideTitles(
-                      showTitles: true,
-                      getTitles: (value) {
-                        if (value.toInt() >= 0 && value.toInt() < dates.length) {
-                          return dates[value.toInt()];
-                        }
-                        return '';
-                      },
+                    borderData: FlBorderData(
+                      show: true,
+                      border: Border.all(
+                        color: Colors.grey[300]!,
+                        width: 1,
+                      ),
+                    ),
+                    gridData: FlGridData(
+                      show: false,
+                    ),
+                    minY: 0,
+                    //추후 1~12월 거래액 중 최대값을 maxY에 넣기
+                    maxY: 2500,
+                    barGroups: List.generate(
+                      months.length,
+                          (index) => BarChartGroupData(
+                        x: index,
+                        barRods: [
+                          BarChartRodData(
+                            y: earnings[index],
+                            width: 16,
+                            colors: [Colors.amber],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  borderData: FlBorderData(show: true),
-                  minY: 1000,
-                  maxY: 2500,
-                  barGroups: dates
-                      .asMap()
-                      .entries
-                      .map(
-                        (entry) => BarChartGroupData(
-                      x: entry.key,
-                      barRods: [
-                        BarChartRodData(
-                          y: earnings[entry.key],
-                          width: 16,
-                          colors: [Colors.amber],
-                        ),
-                      ],
-                    ),
-                  )
-                      .toList(),
                 ),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildStatRow(String label, double value) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          '\$${value.toStringAsFixed(2)}',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
