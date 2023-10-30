bff29d44c95946eb438dae245411f35dea1c1492
yny <skdus2995@naver.com>
2023-10-30 21:45:50 +0900

    expert page 수익관리 페이지

--- lib/expert/my_expert.dart
+++ lib/expert/my_expert.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+
+//페이지
 import 'package:project_flutter/expert/ratings.dart';
 import 'package:project_flutter/expert/revenue.dart';
 import 'package:project_flutter/expert/vacation.dart';
-import 'package:project_flutter/join/userModel.dart';
-import 'package:provider/provider.dart';
 
-import '../join/login_email.dart';
+//세션 관리
+//import 'package:project_flutter/join/userModel.dart';
+//import 'package:provider/provider.dart';
+
 import 'My Portfolio.dart';
 import 'adManagement.dart';
 import 'adRequest.dart';
@@ -13,242 +16,201 @@ import 'messageResponse.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: MyPageScreen(),
+    home: MyExpert(),
+    // 이 외에 필요한 MaterialApp 설정들을 여기에 추가할 수 있습니다.
   ));
 }
 
-class MyPageScreen extends StatefulWidget {
+
+class MyExpert extends StatefulWidget {
 
   @override
-  State<MyPageScreen> createState() => _MyPageScreenState();
+  State<MyExpert> createState() => _MyExpertState();
 }
 
-class _MyPageScreenState extends State<MyPageScreen> {
-
-  late UserModel um;
-
-  @override
-  void initState() {
-    super.initState();
-//context는 build 메서드 내에 사용해야하며, 클래스레벨에서 사용하려면 initState 사용하여 초기화 필요
-    um = Provider.of<UserModel>(context, listen: false);
-  }
+class _MyExpertState extends State<MyExpert> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('마이페이지'),
-        backgroundColor: Color(0xFF00C695), // 초록색
-        titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white), // 흰색 폰트
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.notifications, color: Colors.white), // 알림 아이콘
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.settings, color: Colors.white), // 설정 아이콘
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // 프로필 정보 섹션
-            Center(
-              child: Container(
+        appBar: AppBar(
+          title: Text('마이페이지'),
+          backgroundColor: Color(0xFF00C695), // 초록색
+          titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white), // 흰색 폰트
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.notifications, color: Colors.white), // 알림 아이콘
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.settings, color: Colors.white), // 설정 아이콘
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // 프로필 정보 섹션
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: AssetImage('assets/dog4.png'),
+                        radius: 50,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '사용자 닉네임',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '사용자 등급',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey, // 회색 텍스트
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // 보낸 제안 섹션
+              Container(
                 padding: EdgeInsets.all(16.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/dog4.png'),
-                      radius: 50,
+                    Text(
+                      '보낸 제안',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    // 작업 가능한 프로젝트 목록
+                    // 프로젝트 보러가기 버튼
+                  ],
+                ),
+              ),
+
+              // 판매 정보 섹션
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '판매 정보',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
                     SizedBox(height: 10),
                     Text(
-                      '사용자 닉네임',
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      '3개월 이내 판매중인 건수:',
+                      style: TextStyle(fontSize: 18),
                     ),
                     Text(
-                      '사용자 등급',
+                      '50',
                       style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.grey, // 회색 텍스트
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue, // 파란색 텍스트
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
 
-            // 보낸 제안 섹션
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '보낸 제안',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  // 작업 가능한 프로젝트 목록
-                  // 프로젝트 보러가기 버튼
-                ],
-              ),
-            ),
-
-            // 판매 정보 섹션
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '판매 정보',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    '3개월 이내 판매중인 건수:',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    '50',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue, // 파란색 텍스트
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            // 나의 서비스 섹션
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '나의 서비스',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.monetization_on), // 아이콘 추가
-                    title: Text(
-                      '수익 관리',
+              // 나의 서비스 섹션
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '나의 서비스',
                       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    onTap:(){
-                      if (um.isLogin) {
-                        // 사용자가 로그인한 경우에만 MyPage로 이동
+                    ListTile(
+                      leading: Icon(Icons.monetization_on), // 아이콘 추가
+                      title: Text(
+                        '수익 관리',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      onTap:(){
                         Navigator.of(context).push(MaterialPageRoute(builder: (context) => Revenue()));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-                      }
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.ad_units), // 아이콘 추가
-                    title: Text(
-                      '광고 관리',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      },
                     ),
-                    onTap:(){
-                      if (um.isLogin) {
-                        // 사용자가 로그인한 경우에만 MyPage로 이동
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => AdManagement()));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-                      }
-                    },
-                  ),
+                    ListTile(
+                      leading: Icon(Icons.ad_units), // 아이콘 추가
+                      title: Text(
+                        '광고 관리',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      onTap:(){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => AdManagement()));
+                      },
+                    ),
 
-                  ListTile(
-                    leading: Icon(Icons.add), // 아이콘 추가
-                    title: Text(
-                      '광고 신청',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ListTile(
+                      leading: Icon(Icons.add), // 아이콘 추가
+                      title: Text(
+                        '광고 신청',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      onTap:(){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => AdRequest()));
+                      },
                     ),
-                    onTap:(){
-                      if (um.isLogin) {
-                        // 사용자가 로그인한 경우에만 MyPage로 이동
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => AdRequest()));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-                      }
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.beach_access), // 아이콘 추가
-                    title: Text(
-                      '휴가 설정',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ListTile(
+                      leading: Icon(Icons.beach_access), // 아이콘 추가
+                      title: Text(
+                        '휴가 설정',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      onTap:(){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => Vacation()));
+                      },
                     ),
-                    onTap:(){
-                      if (um.isLogin) {
-                        // 사용자가 로그인한 경우에만 MyPage로 이동
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Vacation()));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
+                    ListTile(
+                      leading: Icon(Icons.star), // 아이콘 추가
+                      title: Text(
+                        '나의 전문가 등급',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      onTap:() {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (
+                            context) => ExpertRating()));
                       }
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.star), // 아이콘 추가
-                    title: Text(
-                      '나의 전문가 등급',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    onTap:(){
-                      if (um.isLogin) {
-                        // 사용자가 로그인한 경우에만 MyPage로 이동
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => ExpertRating()));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-                      }
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.portrait), // 아이콘 추가
-                    title: Text(
-                      '나의 포트폴리오',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ListTile(
+                      leading: Icon(Icons.portrait), // 아이콘 추가
+                      title: Text(
+                        '나의 포트폴리오',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      onTap:(){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => Portfolio()));
+                      },
                     ),
-                    onTap:(){
-                      if (um.isLogin) {
-                        // 사용자가 로그인한 경우에만 MyPage로 이동
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Portfolio()));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-                      }
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.message), // 아이콘 추가
-                    title: Text(
-                      '메시지 응답 관리',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ListTile(
+                      leading: Icon(Icons.message), // 아이콘 추가
+                      title: Text(
+                        '메시지 응답 관리',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      onTap:(){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => MessageResponse()));
+                      },
                     ),
-                    onTap:(){
-                      if (um.isLogin) {
-                        // 사용자가 로그인한 경우에만 MyPage로 이동
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => MessageResponse()));
-                      } else {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
-                      }
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
--- lib/expert/revenue.dart
+++ lib/expert/revenue.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class Revenue extends StatefulWidget {
   @override
@@ -6,7 +7,8 @@ class Revenue extends StatefulWidget {
 }
 
 class _RevenueState extends State<Revenue> {
-  // Declare state variables and methods for managing earnings data
+  List<String> dates = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
+  List<double> earnings = [1000, 1500, 1200, 2200, 1800, 2100, 2300];
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +17,65 @@ class _RevenueState extends State<Revenue> {
         title: Text('수익 관리'),
       ),
       body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            // Earnings summary section
+            Text('수익 그래프'),
             Container(
-              // Display earnings summary information here
-            ),
-
-            // Transaction history section
-            Container(
-              // Display transaction history (e.g., a list of earnings)
+              height: 300,
+              child: BarChart(
+                BarChartData(
+                  gridData: FlGridData(show: false),
+                  titlesData: FlTitlesData(
+                    leftTitles: SideTitles(
+                      showTitles: true,
+                      getTitles: (value) {
+                        double minEarnings = 1000;
+                        double maxEarnings = 2500;
+                        for (var i = minEarnings; i <= maxEarnings; i += 500) {
+                          if (value == i) {
+                            return i.toStringAsFixed(0);
+                          }
+                        }
+                        return '';
+                      },
+                    ),
+                    bottomTitles: SideTitles(
+                      showTitles: true,
+                      getTitles: (value) {
+                        if (value.toInt() >= 0 && value.toInt() < dates.length) {
+                          return dates[value.toInt()];
+                        }
+                        return '';
+                      },
+                    ),
+                  ),
+                  borderData: FlBorderData(show: true),
+                  minY: 1000,
+                  maxY: 2500,
+                  barGroups: dates
+                      .asMap()
+                      .entries
+                      .map(
+                        (entry) => BarChartGroupData(
+                      x: entry.key,
+                      barRods: [
+                        BarChartRodData(
+                          y: earnings[entry.key],
+                          width: 16,
+                          colors: [Colors.amber],
+                        ),
+                      ],
+                    ),
+                  )
+                      .toList(),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      // Add navigation and other necessary elements here
     );
   }
 }
