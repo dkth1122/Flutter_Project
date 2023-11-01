17076d6aa42bd8c54d6c6f4e32829b672f63544d
aji_kim <kaj981020@gmail.com>
2023-11-01 17:28:32 +0900

    아지 > 11/1 상품 결제페이지 만들고 쿠폰 기능 만들기

--- lib/product/productPayment.dart
+++ lib/product/productPayment.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:project_flutter/join/userModel.dart';
 import 'package:provider/provider.dart';
 
@@ -22,6 +23,9 @@ class ProductPayment extends StatefulWidget {
 class _ProductPaymentState extends State<ProductPayment> {
 
   late Stream<QuerySnapshot>? productStream;
+  String selectedCoupon = '--------------------';
+  double discountPercentage = 0.0;
+  int discountedPrice = 0;
 
   @override
   void initState() {
@@ -82,24 +86,109 @@ class _ProductPaymentState extends State<ProductPayment> {
         ),
       ],
     )
-        : Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        : Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          '${widget.productName}',
+          style: const TextStyle(fontSize: 20),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          '상품가: ${NumberFormat('###,###').format(int.parse(widget.price))}원',
+          style: const TextStyle(fontSize: 16),
+        ),
+        Divider(color: Colors.grey),
+        Row(
           children: [
-            Text(
-              '상품명: ${widget.productName}',
-              style: const TextStyle(fontSize: 20),
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: Text(
+                '할인 쿠폰: ',
+                style: TextStyle(fontSize: 16),
+              ),
             ),
-            const SizedBox(height: 10),
-            Text(
-              '가격: ${widget.price} 원',
-              style: const TextStyle(fontSize: 16),
+            SizedBox(width: 8.0),
+            FutureBuilder<QuerySnapshot>(
+              future: FirebaseFirestore.instance.collection('coupon').where('userId', isEqualTo: user).get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final coupons = snapshot.data!.docs;
+                  return DropdownButton<String>(
+                    value: selectedCoupon,
+                    items: [
+                      DropdownMenuItem<String>(
+                        value: '--------------------',
+                        child: Text('--------------------'),
+                      ),
+                      ...coupons.map((coupon) {
+                        return DropdownMenuItem<String>(
+                          value: coupon['cName'],
+                          child: Text(coupon['cName']),
+                        );
+                      }).toList(),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        selectedCoupon = value!;
+                        if (value == '--------------------') {
+                          discountPercentage = 0.0;
+                        } else {
+                          final selectedCouponData = coupons.firstWhere((coupon) => coupon['cName'] == value);
+                          discountPercentage = (int.parse(widget.price) / 100) * (selectedCouponData['discount']).toDouble();
+                          calculateDiscountedPrice();
+                        }
+                      });
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('데이터를 가져오는 동안 오류가 발생했습니다.');
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
             ),
-            // ... 결제 페이지의 내용 추가 ...
           ],
         ),
-      ),
+        const SizedBox(height: 10),
+        Center(
+          child: Text(
+            '상품가: ${NumberFormat('###,###').format(int.parse(widget.price))}원  할인가: ${NumberFormat('###,###').format(discountPercentage)}원 ',
+            style: const TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Center(
+          child: Text(
+            '총 ${NumberFormat('###,###').format(int.parse(widget.price) - discountPercentage)}원',
+            style: const TextStyle(fontSize: 30),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(height: 10), // 마진 추가
+        Divider(color: Colors.grey),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // TODO: 결제 처리를 위한 함수 호출 또는 네비게이션 추가
+            },
+            child: Text(
+              '결제하기',
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xfff48752),
+            ),
+          ),
+        ),
+      ],
+    ),
       ),
     );
+  }
+
+  void calculateDiscountedPrice() { // 할인된 가격 결제시 넘길 금액
+    discountedPrice = int.parse(widget.price) - discountPercentage.toInt();
+    print(discountedPrice);
   }
 }
