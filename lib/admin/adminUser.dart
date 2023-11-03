966a3a6e7e5e93bd85cea07d3e6fd33979b6b36b
aji_kim <kaj981020@gmail.com>
2023-11-03 12:54:47 +0900

    아지 > 11/3 전체 날아간 파일 복구

--- lib/admin/adminUser.dart
+++ lib/admin/adminUser.dart
@@ -11,6 +11,7 @@ class AdminUser extends StatefulWidget {
 
 class _AdminUserState extends State<AdminUser> {
   late Stream<QuerySnapshot> userListStream;
+  late TextEditingController searchUser = TextEditingController();
 
   @override
   void initState() {
@@ -18,8 +19,8 @@ class _AdminUserState extends State<AdminUser> {
     Firebase.initializeApp().then((value) {
       setState(() {
         userListStream = FirebaseFirestore.instance.collection('userList')
-            .where('name', isNotEqualTo: '관리자계정') // '관리자계정'인 경우 제외
-            .orderBy('name') // 이름(name) 기준으로 오름차순 정렬
+            .where('name', isNotEqualTo: '관리자계정')
+            .orderBy('name')
             .snapshots();
       });
     });
@@ -32,55 +33,97 @@ class _AdminUserState extends State<AdminUser> {
         title: Text('회원 관리'),
         backgroundColor: Color(0xFF4E598C),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: userListStream,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('데이터를 불러오는 중에 오류가 발생했습니다.'));
-          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return Center(child: Text('회원이 없습니다.'));
-          } else {
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-                String userId = document.id;
-                String name = data['name'] ?? '이름 없음';
-                String nick = data['nick'] ?? '';
-                String email = data['email'];
-                String birth = data['birth'];
-                Timestamp timestamp = data['cdatetime'];
-                String cdatetime = timestamp.toDate().toString();
-                String banYn = data['banYn'];
-                String delYn = data['delYn'];
-                String status = data['status'];
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchUser,
+              decoration: InputDecoration(
+                hintText: '유저의 ID를 입력하세요',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    String searchKeyword = searchUser.text.trim();
+                    print(searchKeyword);
+                    print('검색어: $searchKeyword');
+                    setState(() {
+                      userListStream = FirebaseFirestore.instance.collection('userList')
+                          .where('userId', isGreaterThanOrEqualTo: searchKeyword)
+                          .where('userId', isLessThan: searchKeyword + 'z')
+                          .orderBy('userId')
+                          .snapshots();
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: userListStream,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('데이터를 불러오는 중에 오류가 발생했습니다.'));
+                } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return Center(child: Text('해당 검색어에 해당하는 회원이 없습니다.'));
+                } else {
+                  return ListView(
+                    children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                      String userId = document.id;
+                      String uId = data['userId'];
+                      String name = data['name'] ?? '이름 없음';
+                      String nick = data['nick'] ?? '';
+                      String email = data['email'];
+                      String birth = data['birth'];
+                      Timestamp timestamp = data['cdatetime'];
+                      String cdatetime = timestamp.toDate().toString();
+                      String banYn = data['banYn'];
+                      String delYn = data['delYn'];
+                      String status = data['status'];
 
-                return ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Text(name, style: TextStyle(color: Colors.black)),
-                      Text(' ($nick)', style: TextStyle(color: Colors.grey)),
-                    ],
-                  ),
-                  subtitle: Text(userId),
-                  trailing: IconButton(
-                    icon: Icon(Icons.search_outlined),
-                    onPressed: () {
-                      AdminUserView user = AdminUserView(userId, name, nick, email, birth, cdatetime, banYn, delYn, status,);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AdminUserViewPage(user: user),
+                      return ListTile(
+                        title: Row(
+                          children: [
+                            Text(name, style: TextStyle(color: Colors.black)),
+                            Text(' ($nick)', style: TextStyle(color: Colors.grey)),
+                          ],
+                        ),
+                        subtitle: Text(userId),
+                        trailing: IconButton(
+                          icon: Icon(Icons.remove_red_eye),
+                          onPressed: () {
+                            AdminUserView user = AdminUserView(
+                              userId,
+                              uId,
+                              name,
+                              nick,
+                              email,
+                              birth,
+                              cdatetime,
+                              banYn,
+                              delYn,
+                              status,
+                            );
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AdminUserViewPage(user: user),
+                              ),
+                            );
+                          },
                         ),
                       );
-                    },
-                  ),
-                );
-              }).toList(),
-            );
-          }
-        },
+                    }).toList(),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
