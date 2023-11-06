5a63ee7a55d5b2a1458baf989fd20570ca4f0e36
eksqjf <129019461+eksqjf@users.noreply.github.com>
2023-11-06 18:58:21 +0900

    bottomBar textButton update commit

--- lib/test.dart
+++ lib/test.dart
@@ -1,33 +1,70 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Test(),
+    // 테스트 스크린을 앱의 홈 화면으로 설정합니다.
+  ));
+}
+
 class Test extends StatefulWidget {
   const Test({Key? key}) : super(key: key);
 
   @override
-  State<Test> createState() => _TestState();
+  _TestState createState() => _TestState();
 }
 
 class _TestState extends State<Test> {
+  int currentPage = 0;
+  final List<String> tutorialPages = [
+    "페이지 1 내용",
+    "페이지 2 내용",
+    "페이지 3 내용",
+  ];
+
+  void nextPage() {
+    if (currentPage < tutorialPages.length - 1) {
+      setState(() {
+        currentPage++;
+      });
+    }
+  }
+
+  void previousPage() {
+    if (currentPage > 0) {
+      setState(() {
+        currentPage--;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("테스트")),
-      body: SingleChildScrollView(
+      appBar: AppBar(title: Text("테스트 튜토리얼")),
+      body: Center(
         child: Column(
-          children: [
-            TextField(),
-            SizedBox(height: 10,),
-            TextFormField(
-              maxLines: 20,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-              ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              tutorialPages[currentPage],
+              style: TextStyle(fontSize: 24.0),
             ),
-            SizedBox(height: 10,),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('버튼'),
-            )
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: previousPage,
+                  child: Text("이전"),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: nextPage,
+                  child: Text("다음"),
+                ),
+              ],
+            ),
           ],
         ),
       ),
