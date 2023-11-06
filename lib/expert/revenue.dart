f511ac8bbdfa7af86d295301adef5ce766b53842
dkth <skdus2995@naver.com>
2023-11-06 12:54:57 +0900

    revenue 수정 중

--- lib/expert/revenue.dart
+++ lib/expert/revenue.dart
@@ -17,9 +17,10 @@ class _RevenueState extends State<Revenue> {
 
   List<double> earnings = List.generate(12, (index) => 0.0);
   double availableEarnings = 0.0;
-  double completedWithdrawals = 500.0;
+  double completedWithdrawals = 0.0;
 
   List<int> prices = [];
+  List<int> prices2 = [];
   List<String> productNames = [];
   List<DateTime> timestamps = [];
 
@@ -56,9 +57,24 @@ class _RevenueState extends State<Revenue> {
       timestamps.add(timestampDateTime);
     }
 
-    availableEarnings = prices.reduce((a, b) => a + b).toDouble();
     updateEarningsData();
   }
+
+  //출금 가능 내역 따로
+  Future<void> fetchPrices() async {
+    final orderCollection = _firestore.collection('orders');
+    final orderQuery = await orderCollection.where('seller', isEqualTo: user).where('withdraw', isNotEqualTo: 'Y').get();
+    final orderDocs = orderQuery.docs;
+
+    for (QueryDocumentSnapshot orderDoc in orderDocs) {
+      int price2 = orderDoc['price'] as int;
+      prices2.add(price2);
+    }
+
+    // prices2 리스트에는 'withdraw' 필드가 'Y'가 아닌 주문의 가격만 저장
+    availableEarnings = prices2.reduce((a, b) => a + b).toDouble();
+  }
+
 
   void updateEarningsData() {
     List<double> updatedEarnings = List.generate(12, (index) => 0.0);
@@ -110,13 +126,33 @@ class _RevenueState extends State<Revenue> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       ElevatedButton(
-                        onPressed: () {
-                          // 출금 신청 로직을 추가
+                        child: Text('출금 신청'),
+                        onPressed: () async {
+                          // 출금 신청 시, seller 필드가 user와 같은 문서들을 선택
+                          QuerySnapshot orderQuery = await _firestore
+                              .collection('orders')
+                              .where('seller', isEqualTo: user)
+                              .get();
+
+                          // 선택한 문서들을 업데이트
+                          for (QueryDocumentSnapshot orderDoc in orderQuery.docs) {
+                            await _firestore.collection('orders').doc(orderDoc.id).update({
+                              'withdraw': 'Y',
+                            });
+                          }
+                          // 출금 신청 시, 출금 가능 수익금을 0으로 업데이트
+                          setState(() {
+                            availableEarnings = 0.0;
+                          });
+                          // 출금 신청 시, 출금 완료 수익금을 신청한 금액만큼 업데이트
+                          setState(() {
+                            completedWithdrawals += availableEarnings;
+                          });
+
                         },
                         style: ElevatedButton.styleFrom(
                           primary: Colors.amber,
                         ),
-                        child: Text('출금 신청'),
                       ),
                       SizedBox(width: 10),
                       ElevatedButton(
@@ -173,7 +209,7 @@ class _RevenueState extends State<Revenue> {
                         getTextStyles: (context, value) => TextStyle(
                           color: Colors.black,
                           fontWeight: FontWeight.bold,
-                          fontSize: 14,
+                          fontSize: 10,
                         ),
                         getTitles: (value) {
                           int index = value.toInt();
@@ -231,7 +267,7 @@ class _RevenueState extends State<Revenue> {
           ),
         ),
         Text(
-          '\$${value.toStringAsFixed(2)}',
+          '${value.toInt()} 원',
           style: TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
