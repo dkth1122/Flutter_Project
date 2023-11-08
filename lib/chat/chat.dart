aa3bd04b5260b1571a277d2782655686d1e3c555
dkth <skdus2995@naver.com>
2023-11-08 15:08:57 +0900

    payment 관련 페이지 CSS 수정

--- lib/chat/chat.dart
+++ lib/chat/chat.dart
@@ -1,5 +1,5 @@
   import 'dart:async';
-import 'dart:io';
+  import 'dart:io';
   import 'package:firebase_storage/firebase_storage.dart';
   import 'package:flutter/material.dart';
   import 'package:cloud_firestore/cloud_firestore.dart';
