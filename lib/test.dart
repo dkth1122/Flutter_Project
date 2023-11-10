1c12cf4790a02b617412595026b1f08f92c055f2
eksqjf <129019461+eksqjf@users.noreply.github.com>
2023-11-10 19:22:21 +0900

    test commit

--- lib/test.dart
+++ lib/test.dart
@@ -1,127 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
-import 'main.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
-      home: Test(),
-    ),
-  );
-}
+import 'package:project_flutter/search/searchPortfolioDetail.dart';
 
 class Test extends StatefulWidget {
-  const Test({Key? key});
+  const Test({super.key});
 
   @override
   State<Test> createState() => _TestState();
 }
 
 class _TestState extends State<Test> {
-  int currentPage = 0;
-  double positionTop = 0.0;
-
-  final List<String> tutorialPages = ["1", "2", "3"];
-
-  Future<void> showDialogWithPosition() async {
-    if (mounted) {
-      await showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return Dialog(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            insetPadding: EdgeInsets.all(0),
-            child: Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.transparent,
-                ),
-                Positioned(
-                  top: positionTop,
-                  right: 10,
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height / 2 - 20,
-                  left: MediaQuery.of(context).size.width / 2 - 60,
-                  child: ElevatedButton(
-                    child: Text('이전'),
-                    onPressed: previousPage,
-                  ),
-                ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height / 2 - 20,
-                  left: MediaQuery.of(context).size.width / 2 + 20,
-                  child: ElevatedButton(
-                    child: Text('다음'),
-                    onPressed: nextPage,
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      );
-    }
-  }
-
-
-
-  void nextPage() {
-    if (currentPage < tutorialPages.length - 1) {
-      setState(() {
-        currentPage++;
-        positionTop += 100.0;
-        showDialogWithPosition(); // 위치를 변경한 다음 다시 Dialog를 열기
-      });
-    } else {
-      Navigator.pop(context); // Dialog 닫기
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => HomePage()),
-      );
-    }
-  }
-
-  void previousPage() {
-    if (currentPage > 0) {
-      setState(() {
-        currentPage--;
-        showDialogWithPosition(); // 위치를 변경한 다음 다시 Dialog를 열기
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(milliseconds: 1), () {
-      showDialogWithPosition();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('다이얼로그 테스트'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: showDialogWithPosition,
-          child: Text('다이얼로그 열기'),
+      body: Container(
+        child: Column(
+          children: [
+            _cntPortFolio(),
+          ],
         ),
       ),
     );
   }
+
+  Widget _cntPortFolio() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collectionGroup("portfolio").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+
+        // 데이터를 가져오고 ListView.builder를 사용하여 목록을 만듭니다.
+        var portfolios = snapshot.data!.docs;
+        portfolios.sort((a, b) => (a['cnt'] as int).compareTo(b['cnt'] as int));
+
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: portfolios.length,
+          itemBuilder: (context, index) {
+            var portfolio = portfolios[index].data() as Map<String, dynamic>;
+            // 여기에서 가져온 데이터를 사용하여 원하는 작업을 수행하세요.
+            // 예를 들어, 포트폴리오 정보를 표시하거나 다른 위젯을 만들 수 있습니다.
+            return ListTile(
+              title: Text(portfolio['title']),
+              subtitle: Text('Count: ${portfolio['cnt']}'),
+              onTap: () {
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
 }
