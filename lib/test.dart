7f784323464822592b29b1d278f86b0dd76ab8a1
eksqjf <129019461+eksqjf@users.noreply.github.com>
2023-11-13 10:54:02 +0900

    main commit

--- lib/test.dart
+++ lib/test.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:project_flutter/search/searchPortfolioDetail.dart';
 
 class Test extends StatefulWidget {
-  const Test({super.key});
+  const Test({Key? key});
 
   @override
   State<Test> createState() => _TestState();
@@ -25,7 +25,7 @@ class _TestState extends State<Test> {
 
   Widget _cntPortFolio() {
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collectionGroup("portfolio").snapshots(),
+      stream: FirebaseFirestore.instance.collectionGroup("portfolio").limit(4).snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
@@ -37,7 +37,9 @@ class _TestState extends State<Test> {
 
         // 데이터를 가져오고 ListView.builder를 사용하여 목록을 만듭니다.
         var portfolios = snapshot.data!.docs;
-        portfolios.sort((a, b) => (a['cnt'] as int).compareTo(b['cnt'] as int));
+
+        // sort 함수를 사용하여 cnt를 기준으로 정렬
+        portfolios.sort((a, b) => (b['cnt'] as int).compareTo(a['cnt'] as int));
 
         return ListView.builder(
           shrinkWrap: true,
@@ -45,18 +47,23 @@ class _TestState extends State<Test> {
           itemCount: portfolios.length,
           itemBuilder: (context, index) {
             var portfolio = portfolios[index].data() as Map<String, dynamic>;
-            // 여기에서 가져온 데이터를 사용하여 원하는 작업을 수행하세요.
-            // 예를 들어, 포트폴리오 정보를 표시하거나 다른 위젯을 만들 수 있습니다.
             return ListTile(
               title: Text(portfolio['title']),
               subtitle: Text('Count: ${portfolio['cnt']}'),
               onTap: () {
+                // 클릭했을 때 추가 작업 수행
+                var portfolioId = portfolios[index].id; // "portfolio" 문서의 ID 가져오기
+                var parentCollectionId = portfolios[index].reference.parent!.id; // 상위 컬렉션의 ID 가져오기
+
+                // 이제 portfolioId 및 parentCollectionId를 사용하여 필요한 작업을 수행할 수 있습니다.
+                print("Portfolio ID: $portfolioId");
+                print("Parent Collection ID: $parentCollectionId");
               },
+
             );
           },
         );
       },
     );
   }
-
 }
