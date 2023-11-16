b6a93ca0abbb32456dff213f270313ca38bf1094
dkth <skdus2995@naver.com>
2023-11-16 12:36:24 +0900

    메시지 응답 저장 버튼 수정

--- lib/chat/chatBot.dart
+++ lib/chat/chatBot.dart
@@ -103,7 +103,7 @@ class _ChatResponsePageState extends State<ChatResponsePage> {
       if (isNightResponseEnabled) {
         return "예, 현재는 야간 응답이 가능합니다. \n 23:00~08:00(KST) 동안 야간 응답을 지원합니다.";
       } else {
-        return "죄송합니다, 현재는 야간 응답이 불가능한 시간입니다.";
+        return "죄송합니다. 현재는 야간 응답이 불가능한 시간입니다.";
       }
     } else if (question.contains("휴가 중")) {
       if (isOnVacation) {
@@ -115,7 +115,7 @@ class _ChatResponsePageState extends State<ChatResponsePage> {
       if (isResponseEnabled) {
         return "네, 현재는 바로 응답이 가능한 상태입니다. 30분 내에 응답을 드릴 수 있습니다.";
       } else {
-        return "죄송합니다, 현재는 30분 내에 응답할 수 없는 상태입니다.";
+        return "죄송합니다. \n 현재는 30분 내에 응답할 수 없는 상태입니다.";
       }
     }
     return "안녕하세요! 무엇을 도와드릴까요?";
